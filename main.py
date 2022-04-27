70d707ea2dc9eb3d9e82fe578aadd61e8bfac047
aleksandrkrasnikov <a.s.krasnikov@yandex.ru>
2022-04-27 14:16:05 +0300

    TestisDone

--- main.py
+++ main.py
@@ -1,63 +1,67 @@
-#
-# # parser = ProgHub_parser(driver, "PY")
-# # parser.parse()
-#
-# driver.get("https://zen.yandex.ru/")
-# btn_sing_up = driver.find_element_by_class_name("auth-header-buttons-view__right-link")
-# btn_sing_up.click()
-#
-# btn_sing_in = driver.find_element_by_class_name("registration-auth-link__link")
-# btn_sing_in.click()
 
 import time
 
-# webdriver это и есть набор команд для управления браузером
 from selenium import webdriver
 
-# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-
-# команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
-time.sleep(5)
+time.sleep(2)
 
 # Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
-driver.get("https://mail.yandex.ru/?noretpath=1")
-enter = driver.find_element(By.XPATH, '//*[@id="index-page-container"]/div/div[2]/div/div/div[4]/a[2]')
-enter.click()
-time.sleep(2)
-# mail = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[1]/div[2]/button')
-# mail.click()
-enter1 = driver.find_element(By.XPATH, '//*[@id="passp-field-login"]')
-enter1.send_keys ("testkrasnikov@yandex.ru")
-enter2 = driver.find_element(By.XPATH, '//*[@id="passp:sign-in"]')
-enter2.click()
+driver.get("https://passport.yandex.ru/auth/add?from=mail&origin=hostroot_homer_auth_ru&retpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fnoretpath%3D1&backpath=https%3A%2F%2Fmail.yandex.ru%3Fnoretpath%3D1")
 time.sleep(1)
+# Ищем поля ввода адреса почты
+username = driver.find_element(By.XPATH, '//*[@id="passp-field-login"]')
+# Вводим почту
+username.send_keys("testkrasnikov@yandex.ru")
+# Ищем кнопку Войти
+button = driver.find_element(By.XPATH, '//*[@id="passp:sign-in"]')
+# Кликаем на войти
+button.click()
+# Ждем 1 секунду
+time.sleep(1)
+# Ищем поле для ввода пароля
 password = driver.find_element(By.ID, "passp-field-passwd")
+# Вводим пароль
 password.send_keys("27Avg1986I")
-
+# Ждем 1 секунду
 time.sleep(1)
-
+# Ищем кнопку Войти
 enter3 = driver.find_element(By.XPATH, '//*[@id="passp:sign-in"]')
+# Жмем на кнопку войти
 enter3.click()
-
-time.sleep(30)
-# driver.quit()
-# Метод find_element_by_css_selector позволяет найти нужный элемент на сайте, указав путь к нему. Способы поиска элементов мы обсудим позже
-# Ищем поле для ввода текста
-# textarea = driver.find_element_by_css_selector(".textarea")
-
-# Напишем текст ответа в найденное поле
-# textarea.send_keys("get()")
-# time.sleep(5)
-
-# Найдем кнопку, которая отправляет введенное решение
-# submit_button = driver.find_element_by_css_selector(".submit-submission")
-
-# Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
-# submit_button.click()
-# time.sleep(5)
-
-# После выполнения всех действий мы не должны забыть закрыть окно браузера
-
+# Ждем 5 секунд чтобы все загрузилось
+time.sleep(5)
+# Ищем кнопку написать
+toWrite = driver.find_element(By.XPATH, '//*[@id="js-apps-container"]/div[2]/div[7]/div/div[3]/nav/div[3]/div/div/div/a')
+# Жмем на кнопку написать
+toWrite.click()
+# Ждем 1 секунду чтобы появилось окно
+time.sleep(1)
+# Ищем поле для ввода почты кому
+message = driver.find_element(By.XPATH, '//*[@id="js-apps-container"]/div[2]/div[10]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[1]/div[1]/div/div/div/div/div')
+# Вводим почту
+message.send_keys('a.s.krasnikov@yandex.ru')
+time.sleep(1)
+# Ищем поле ввода сообщения
+text = driver.find_element(By.XPATH, '//*[@id="cke_1_contents"]/div')
+# Жмем на поле ввода сообщения
+text.click()
+# Вводим сообщение
+text.send_keys('Отправляем тестовое сообщение')
+time.sleep(1)
+# Ищем поле с типом файл-инпут
+file = driver.find_element(By.XPATH, '//*[@id="js-apps-container"]/div[2]/div[10]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/span/span/div/input')
+# Добавляем файл с локального компьютера
+file.send_keys('/Users/aleksandrkrasnikov/Downloads/Test.txt')
+# Ждем 5 секунд чтобы файл успешно загрузился, можно увеличить для больших файлов
+time.sleep(5)
+# Ищем кнопку отправить
+buttonSend = driver.find_element(By.XPATH, '//*[@id="js-apps-container"]/div[2]/div[10]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/div[2]/div/div[1]/div[1]/button')
+# Жмем кнопку отправить
+buttonSend.click()
+# Ждем 15 секунд
+time.sleep(15)
+# Закрываем браузер
+driver.quit()
