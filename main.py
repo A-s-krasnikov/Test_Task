93965932a72ae52508caf8c72bb42a895202e8ac
aleksandrkrasnikov <a.s.krasnikov@yandex.ru>
2022-04-28 14:22:16 +0300

    TestisDone

--- main.py
+++ main.py
@@ -19,31 +19,38 @@ username.send_keys("testkrasnikov@yandex.ru")
 button = driver.find_element(By.XPATH, '//*[@id="passp:sign-in"]')
 # Кликаем на войти
 button.click()
+
 # Ждем 1 секунду
 time.sleep(1)
 # Ищем поле для ввода пароля
 password = driver.find_element(By.ID, "passp-field-passwd")
 # Вводим пароль
 password.send_keys("27Avg1986I")
+
 # Ждем 1 секунду
 time.sleep(1)
 # Ищем кнопку Войти
 enter3 = driver.find_element(By.XPATH, '//*[@id="passp:sign-in"]')
 # Жмем на кнопку войти
 enter3.click()
+
 # Ждем 5 секунд чтобы все загрузилось
 time.sleep(5)
+
 # Ищем кнопку написать
 toWrite = driver.find_element(By.XPATH, '//a[@class=\'Button2 Button2_type_link Button2_view_action Button2_size_m Layout-m__compose--3KGCi qa-LeftColumn-ComposeButton ComposeButton-m__root--3ijKP\']')
 # Жмем на кнопку написать
 toWrite.click()
+
 # Ждем 1 секунду чтобы появилось окно
 time.sleep(1)
+
 # Ищем поле для ввода почты кому
 message = driver.find_element(By.XPATH, '//div[@class=\'composeYabbles\']')
 # Вводим почту
 message.send_keys('a.s.krasnikov@yandex.ru')
 time.sleep(1)
+
 # Ищем поле ввода сообщения
 text = driver.find_element(By.XPATH, '//*[@id="cke_1_contents"]/div')
 # Жмем на поле ввода сообщения
@@ -51,6 +58,7 @@ text.click()
 # Вводим сообщение
 text.send_keys('Отправляем тестовое сообщение')
 time.sleep(1)
+
 # Ищем поле с типом файл-инпут
 file = driver.find_element(By.XPATH, '//input[@class="WithUpload-FileInput qa-Compose-FileInput"]')
 # Добавляем файл с локального компьютера
